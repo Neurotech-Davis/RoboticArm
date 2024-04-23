8ee41d13a4729db244c806bc1895db65720f5ca6
EvanSilvey <115114148+EvanSilvey@users.noreply.github.com>
2024-04-23 16:12:34 -0700

    Update neutral.py
    Fixed Baudrate and added ability to read Arduino output

--- Mental_Command_Scripts/neutral.py
+++ Mental_Command_Scripts/neutral.py
@@ -1,7 +1,13 @@
 # Importing Libraries 
 from serial import Serial
 import time 
-arduino = Serial(port='COM10', baudrate=96000, timeout=1)
+arduino = Serial(port='COM10', baudrate=9600, timeout=1)
 
-arduino.write('1'.encode('utf-8')) 
+arduino.write('5'.encode('utf-8'))
+
+try:
+    boardData = connection.readline().decode('ascii')
+except UnicodeDecodeError:
+    boardData = connection.readline().decode('ascii')
+
 arduino.close()
