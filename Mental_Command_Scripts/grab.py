153872dd7c20b6b4fa1e7cb9852fecc07f72129f
AhmedSey03 <152769132+AhmedSey03@users.noreply.github.com>
2024-04-23 21:59:00 -0700

    new changes

--- Mental_Command_Scripts/grab.py
+++ Mental_Command_Scripts/grab.py
@@ -1,13 +1,16 @@
 # Importing Libraries 
 from serial import Serial
 import time 
-arduino = Serial(port='COM10', baudrate=9600, timeout=1)
+arduino = Serial(port='COM3', baudrate=9600, timeout=1)
+while True:
+    
+    arduino.write('1'.encode('utf-8'))
 
-arduino.write('1'.encode('utf-8'))
-
-try:
-    boardData = connection.readline().decode('ascii')
-except UnicodeDecodeError:
-    boardData = connection.readline().decode('ascii')
-
-time.sleep(1)
+    try:
+        boardData = arduino.readline().decode('ascii')
+    except UnicodeDecodeError:
+        boardData = arduino.readline().decode('ascii')
+    print(boardData)
+    if boardData == '1':
+        break
+    time.sleep(1)
--- Mental_Command_Scripts/neutral.py
+++ Mental_Command_Scripts/neutral.py
@@ -1,13 +1,15 @@
 # Importing Libraries 
 from serial import Serial
 import time 
-arduino = Serial(port='COM10', baudrate=9600, timeout=1)
+arduino = Serial(port='COM3', baudrate=9600, timeout=1)
+while True:
+    arduino.write('5'.encode('utf-8'))
 
-arduino.write('5'.encode('utf-8'))
-
-try:
-    boardData = connection.readline().decode('ascii')
-except UnicodeDecodeError:
-    boardData = connection.readline().decode('ascii')
-
-arduino.close()
+    try:
+        boardData = arduino.readline().decode('ascii')
+    except UnicodeDecodeError:
+        boardData = arduino.readline().decode('ascii')
+    print(boardData)
+    if boardData == '5':
+        break
+    arduino.close()
